430d1f7177ff3efee49022dcc129c12a6f1a07f5
Eduardo <eduardo30091992@gmail.com>
2020-06-14 18:10:41 -0500

    1003 - Se configura el formulario de webinar

---
+++ validaWebinar.php
@@ -0,0 +1,28 @@
+<?php
+
+	/*$nombre = $_POST['nombre']; 
+    $compania = $_POST['compania']; 
+    $tel = $_POST['tel']; 
+    $correo= $_POST['correo'];
+    $desc = $_POST['descripcion'];
+    
+    echo "Respuesta de php: ".$nombre." ".$compania." ".$tel." ".$correo." ".$desc;
+    */  
+    if(empty($_POST['nombre']) || empty($_POST['compania']) || empty($_POST['tel']) || empty($_POST['correo']) || empty($_POST['descripcion'])) {//si estan vacios
+        echo "false";
+    }else{
+        $nombre = $_POST['nombre']; 
+        $compania = $_POST['compania']; 
+        $tel = $_POST['tel']; 
+        $correo= $_POST['correo'];
+        $desc = $_POST['descripcion']; 
+        //creacion de txt
+        $contenido=" Nombre: $nombre Empresa: $compania Telefono $tel Correo: $correo Descripcion: $desc";
+        $archivo = "";
+        $archivo=fopen("txt-webinar/clientes.txt","a");
+        // escribimos la informacion 
+        fwrite ($archivo,$contenido);
+        echo "https://www.imaginanet.com/blog/usando-ajax-un-simple-ejemplo-con-php-y-jquery.html";
+    }
+
+?>
--- webinar_proximos.php
+++ webinar_proximos.php
@@ -1,114 +1,139 @@
 <?php
 		include("barra.php");
   ?>
-  <!---------------------- html------------------------------------------>
 
-  <script>
+<script type="text/javascript">
 function enviar(){
-  alert("nuevo");
-}
 
+  var response = grecaptcha.getResponse();
+  /*if(response.length == 0){
+      console.log("captcha no");
+  }else{
+    console.log("captcha");
+  }*/
+  var nombre = document.getElementById("nombre").value;
+  var compania = document.getElementById("compania").value;
+  var tel = document.getElementById("tel").value;
+  var email = document.getElementById("email").value;
+  var descripcion = document.getElementById("descripcion").value;
+  if(nombre.length > 0 && compania.length > 0 && tel.length > 0 && email.length > 0 && descripcion.length && !(response.length == 0)){
+    if(!validaTelefono(tel)){
+      alert("Ingrese un telefono valido");
+    }else if(!validarEmail(email)){
+      alert("Ingrese un correo valido");
+    }else{
+      var parametros = {
+        "nombre":nombre,
+        "compania":compania,
+        "tel":tel,
+        "correo":email,
+        "descripcion":descripcion
+      };
+      $.ajax({
+        data: parametros,
+        url:"validaWebinar.php",
+        type:"POST",
+        beforeSend: function(){
+          console.log("Procesando");
+        },
+        success: function(data){
+          if(!(data == "false")){
+            console.log("url:"+data); 
+            limpiar();
+            document.getElementById("idPop").innerHTML ='<a href="'+data+'" target="_blank">Click url webinar</a><p>Url webinar: '+data+'</p>';
+            openModal();
+          }
+        }
+      });
+    }
+  }else{
+    alert("Favor de llenar todos los campos");
+  }
+}
+function limpiar(){
+  var nombre = document.getElementById("nombre").value ="";
+  var compania = document.getElementById("compania").value="";
+  var tel = document.getElementById("tel").value="";
+  var email = document.getElementById("email").value="";
+  var descripcion = document.getElementById("descripcion").value="";
+  grecaptcha.reset();
+}
+function openModal(){
+  $("#pop-webinar").addClass("pop-visible");
+}
+function closeModal(){
+  $("#pop-webinar").removeClass("pop-visible");
+}
+function validarEmail(valor) {
+  if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(valor)){
+   return true;
+  } else {
+   return false;
+  }
+}
+function validaTelefono(valor) {
+  if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(valor)){
+   return true;
+  } else {
+   return false;
+  }
+}
 </script>
 
 
-
- <!---------------------- Proximos Webinar ----------------------------->
- <div class="proximos-webinar">
-    <div class="container">
+<div class="proximos-webinar">
+  <div class="container">
     <div class="left-side">
-    <img src="imagen/webinar/webinar1.png" class="img-fluid" alt="imagen webinar" width="100%">  
+      <img src="imagen/webinar/webinar1.png" class="img-fluid" alt="imagen webinar" width="100%">  
     </div>
-<!---------------- contacto ----------------------------------------->
-  
+  </div>
+</div>
+<!-- contacto -->
 <div class="right-side">
-					<div class="contacto-webinar">
-				<div class="bloque1">
+	<div class="contacto-webinar">
+		<div class="bloque1">
 					<div class="registrate">
 						<p>Registrate para obtener el link</p>
 					</div>
 					<form name="formu" class="forms" >
-						<p><input class="input-webinar" type="text" name="nombre" placeholder="Nombre completo" required></p>
-						<p><input class="input-webinar" type="text" name="compania" placeholder="Empresa" required></p>
-						<p><input class="input-webinar" type="tel" name="tel" placeholder="Teléfono" required></p>
-						<p><input class="input-webinar" type="email" name="correo" placeholder="Correo Electrónico" required></p>
-						<p><textarea class="text-webinar" type="text" name="descripcion" placeholder="Cuéntanos como te podría ayudar esta herramienta" required></textarea></p>
+						<p><input class="input-webinar" type="text" id="nombre" name="nombre" placeholder="Nombre completo" required /></p>
+						<p><input class="input-webinar" type="text" id="compania" name="compania" placeholder="Empresa" required /></p>
+						<p><input class="input-webinar" type="tel" id="tel" name="tel" placeholder="Teléfono" required /></p>
+						<p><input class="input-webinar" type="email" id="email" name="correo" placeholder="Correo Electrónico" required /></p>
+						<p><textarea class="text-webinar" type="text" id="descripcion" name="descripcion" placeholder="Cuéntanos como te podría ayudar esta herramienta" required></textarea></p>
             <div class="g-recaptcha" data-sitekey="6LesaPYUAAAAAOU62bIR5KB5cGk5tX5L2JJUYQvP"></div>
             <br>
-        	<p><input type="button" onclick="enviar()" id="botoncontacto" name="enviar" value="Enviar" class="btn-webinar"></p>
-            <!--<button type="submit" id="botoncontacto" name="enviar" value="Enviar" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#mimodalejemplo"> Enviar</button> -->
-            </form>
-                  
-					<div class="respuesta">
+            <a href="#procesar" onclick="enviar()" id="botoncontacto" name="enviar" class="btn-webinar">Enviar</a>
+          </form>
+			<div class="respuesta">
 
-
-<!-- Metodo Post -->
-
-            <?php
-              if(isset($_POST['enviar'])){  
-                  if(empty($_POST['nombre']) || empty($_POST['compania']) || empty($_POST['tel']) || empty($_POST['correo']) || empty($_POST['descripcion']) || empty($_POST['g-recaptcha-response'])) {//si estan vacios
-                      echo "Por favor, Valida el Captcha";
-                  }else{
-                      //validamos
-                      
-                      function valida_email($a){
-                                if (!preg_match("/^[a-z ]+$/i", stripslashes(trim($a)))) return true;
-                                else return false;                
-                      }
-                      function val_numero($x){
-                         $x=strip_tags($x);
-                         $resultado=0;
-                         if(is_numeric($x)){
-                         $resultado=$x;
-                         return $resultado;
-                         }
-                      }
-                      //if(val_numero($_POST['tel'])){
-                          if(valida_email($_POST['correo'])){
-                          
-                              $nombre = $_POST['nombre']; 
-                              $compania = $_POST['compania']; 
-                              $tel = $_POST['tel']; 
-                              $correo= $_POST['correo'];
-                              $desc = $_POST['descripcion']; 
-                            //creacion de txt
-                              $contenido=" Nombre: $nombre Empresa: $compania Telefono $tel Correo: $correo Descripcion: $desc
-                             ";
-                              // abrimos el archivo
-                              $archivo = "";
-                              $archivo=fopen("txt-webinar/clientes.txt","a");
-                              // escribimos la informacion 
-                              fwrite ($archivo,$contenido);
-                              // ventana emergente popup
-
-                                                        
-                              
-                              /* if(mail($correo,"SOLICITUD DE webinar",$cuerpo)){
-                                  echo "Correo enviado correctamente";
-                              }else{
-                                  echo "No se pudo mandar el correo porfavor intentelo de nuevo Gracias";
-                              }
-                         
-                          }else{
-                            echo "email no valido";
-                          }*/
-                       /*}else{
-                          echo "El campo telefono solo debe de contener valores numericos";
-                       }*/
-                      }
-                  }
-              }
-            ?>
-					</div>
-				</div>
+            
+			</div>
 		</div>
-		<br>
-		<br>
 	</div>
+</div>
 
-<!------------------  aqui termina el formulario ---------------------->
-
+<!-- aqui termina el formulario -->
+<div class="pop-webinar pop-close" id="pop-webinar">
+  <div class="pop-container">
+    <div class="pop-space">
+      <div class="pop-title">
+        <p>URL WEBINAR</p>
+        <span class="close_pop" onclick="closeModal()">x</span>
+      </div>
+      <div class="pop-body">
+        <div class="pop-text" id="idPop">
+          
+        </div>
+        <div class="pop-control">
+          <a href="#close" onclick="closeModal()">Cerrar</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
   
 
 <?php
         include("footer.php");
-    ?>
+?>
